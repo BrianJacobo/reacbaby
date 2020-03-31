cf06262013376577033c2a3213a84d0e17e329be
BrianJacobo <jacobo.brian.fuentes@outlook.com>
2020-03-30 22:03:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+//Importar componente
+import MiComponente from './components/MiComponente';
+
+function habla(nombre,edad){
+  var presentacion = 
+    (<div>
+      <h2>Hola, soy {nombre}</h2>
+      <h3>maik, tengo {edad} años</h3>
+    </div>)
+  return presentacion
+}
+function App() {
+  var nombre = 'Brian Jacobo' 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sa' gente csmr xD
+        </p>
+        {habla(nombre,21)}
+        <section className="componentes">
+        <MiComponente />
+        <MiComponente />
+        <MiComponente />
+        <MiComponente />
+      </section>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
